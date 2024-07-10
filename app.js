340d7e8d347aab50842d235755b1f94d7b30923b
4jss <4jss69@gmail.com>
2024-07-10 22:51:33 +0200

    idk

--- app.js
+++ app.js
@@ -7,22 +7,14 @@ const app = express();
 app.use(express.json());
 app.set('view engine', 'ejs');
 app.use('/', require('./router/main'));
-app.all('/', async(req, res) => {
+app.all('/*', async(req, res) => {
     try {
-        const client = new MongoClient(config.get('env.uri'));
-        const b = req.cookies.token;
-        const decodedb = await jwt.verify(b, config.get('env.secret'));
-        const e = decodedb.username
-        const c = client.db(config.get('env.db')).collection(config.get('env.users_collection')).findOne({ e });
-            
-        if (c !== null) {
-            return res.redirect(`${config.get('server.hostname')}/user/dashboard`);
-        }; 
+        return res.status(404).render('404');
     } catch (error) {
         return (
             console.error(error),
             res.status(500).json({ status: 500, message: 'internal server error' })
-        )
+        );
     }
 });
 
